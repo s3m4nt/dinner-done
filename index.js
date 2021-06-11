cce91824bb2178f538d7a24e8e161213db13894f
Brian Young <berkshirehunt@gmail.com>
2021-06-11 15:48:02 -0700

    new user functionality + FAVE and DELETE buttons

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 
+const db = require('./models')
+
 const express = require('express')
 const axios = require('axios')
 const ejsLayouts = require('express-ejs-layouts')
 const methodOverride = require('method-override')
-
 const app = express()
 const PORT = process.env.PORT || 3000
 
@@ -14,6 +15,8 @@ app.use(ejsLayouts)
 app.use(express.static(__dirname + '/public/'))
 app.use(methodOverride('_method'))
 
+//// Begin Routes
+
 // GET / - main index of site
 app.get('/', (req, res) => {
   // / route gets layout.ejs from views
@@ -22,19 +25,40 @@ app.get('/', (req, res) => {
 
 // GET / - main index of site
 app.get('/recipes', (req, res) => {
-  let themealURL = `http://www.themealdb.com/api/json/v1/1/search.php?i=${req.query.ingredients}`
-  console.log('💀💀💀💀💀💀💀💀',req.query.ingredients)
-  console.log(themealURL);
+
+  // repair syntax at query strings
+  const fixStr = req.query.ingredients
+  const fixStr2 = fixStr.split(',').map(x => x.trim()).map(y => y.replace(/\s/g, '_')).join(',')
+  let themealURL = `http://www.themealdb.com/api/json/v2/${process.env.THEMEALDB_API_KEY}/filter.php?i=${fixStr2}`
   // Use request to call the API
   axios.get(themealURL)
     .then((apiResponse) => {
-      let recipes = apiResponse.data.meals[0]
+      let recipes = apiResponse.data.meals
       // res.json(recipes)
+      if (recipes){
       res.render('recipes', { recipes: recipes })
+    }else{
+      res.send(`There are no recipes containing ${fixStr} :-(`)
+    }
     })
     .catch((err) => {
-      console.log('🔥🔥🔥🔥🔥🔥🔥🔥', err)
+      console.log(err)
     })
+})
+
+app.post('/new-user', (req,res) => {
+// get CREATE USER form data
+const newUser = req.body.userName
+  db.user.create({
+    name: newUser,
+
+  })
+  .then((user) => {
+    res.redirect('/')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 })
 
 app.get('/saved', (req, res) => {
